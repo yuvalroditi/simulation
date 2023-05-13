4e30ee3880512d54f0d1c1c800d5989e683eed89
yuvalroditi <yuval.roditi@mail.huji.ac.il>
2023-05-13 16:53:21 +0300

    finish q2 and q3 and now try to make q2 be efficient

--- assignment_2/311306435_Assignment_2.R
+++ assignment_2/311306435_Assignment_2.R
@@ -2,7 +2,7 @@
 # Name: Yuval Roditi
 # I.D. Number: 311306435
 
-# Q1: Replace "return(NA)" by your code
+# Q1
 a2q1 <- function(n, mu, Sigma){
   p = length(mu)
   eig <- eigen(Sigma, symmetric = TRUE)
@@ -19,12 +19,24 @@ a2q1 <- function(n, mu, Sigma){
 }
 
 
-# Q2: Replace "return(NA)" by your code
+# Q2
 a2q2 <- function(n, m, a, b, sd, sd.0){
-  return(NA)
+  Y0 <- rnorm(n, 0, sd.0)
+  Y <- cbind(Y0)
+  eps <- matrix(rnorm(n * (m+1), 0, sd), nrow=n)
+  for (i in 1:m){
+    Y <- cbind(Y, a*Y[1:n,i] + b*eps[1:n,i] + eps[1:n,i+1])
+  }
+  return(Y[1:n, 1:m+1])
 }
 
-# Q3: Replace "return(NA)" by your code
+# Q3
 a2q3 <- function(n, rN, rX){
-  return(NA)
+  N <- rN(n)
+  S <- numeric(n)
+  for (i in 1:n){
+    X <- rX(N[i])
+    S[i] <- sum(X)
+  }
+  return(S)
 }
