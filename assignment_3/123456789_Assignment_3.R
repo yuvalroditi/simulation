197f09cf5f593ac4632ca31c90e49e1a7a506b17
yuvalroditi <yuval.roditi@mail.huji.ac.il>
2023-06-06 17:19:42 +0300

    q1 works but not efficient

--- assignment_3/123456789_Assignment_3.R
+++ assignment_3/123456789_Assignment_3.R
@@ -4,7 +4,18 @@
 
 # Q1: Replace "return(NA)" by your code
 a3q1 <- function(x, n, p0, p1, N){
-  return(NA)
+  Y <- rbinom(N, n, p1)
+  single_est <- function(a){
+    indicator <- (Y>a)
+    phi <- dbinom(Y, n, p0)
+    psi <- dbinom(Y, n, p1)
+    return (mean(indicator*phi/psi))
+  }
+  # vec_est <-numeric(length(x))
+  # for (i in 1:length(x)){
+  #  vec_est[i] <- single_est(x[i])
+  # }
+  return(sapply(x, single_est))
 }
 
 
