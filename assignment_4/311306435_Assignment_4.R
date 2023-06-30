2dae001f5e3e9b71aa1de43fe3b327bdac38d724
yuvalroditi <yuval.roditi@mail.huji.ac.il>
2023-06-30 17:23:28 +0300

    finished q1, works

--- assignment_4/311306435_Assignment_4.R
+++ assignment_4/311306435_Assignment_4.R
@@ -14,13 +14,42 @@ a4q1 <- function(x, f.alpha, f.lambda, start, N){
   #   return ((al^n)*(lm^(n*al))*prod(x^(al-1))*exp(-lm*sum(x^al)))
   # }
   ll <- function(al, lm, x){
-    return (n*log(al) + (n*al)*log(lm) + (al-1)*sum(log(x)) - (lm^al)*sum(x^al))
+    return (n*log(al) + (n*al)*log(lm) + (al-1)*sum(log(x)) - (lm)*sum(x^al))
   }
   ratio_func <- function(al, lm, al_til, lm_til, x){
     ll_theta_tilde <- ll(al_til, lm_til, x)
     ll_theta <- ll(al, lm, x)
     ll_ratio <- ll_theta_tilde - ll_theta
-    ratio <- (f.alpha (al_til) *f.lambda(lm_til) * exp(ll_ratio))/(f.alpha(al)*f.lambda(lm))
+    tran_al_al_til = dexp(al_til, rate = (1/al))
+    tran_al_til_al = dexp(al, rate = (1/al_til))
+    tran_lm_lm_til = dexp(lm_til, rate = (1/lm))
+    tran_lm_til_lm = dexp(lm, rate = (1/lm_til))
+    # print("--up---")
+    # print(f.alpha (al_til))
+    # print(f.lambda(lm_til))
+    # print(exp(ll_ratio))
+    # print(tran_al_til_al)
+    # print(tran_lm_til_lm)
+    # print("--down--")
+    # print(f.alpha(al))
+    # print(f.lambda(lm))
+    # print(tran_al_al_til)
+    # print(tran_lm_lm_til)
+    numerator <- f.alpha (al_til) *f.lambda(lm_til) * exp(ll_ratio)* tran_al_til_al * tran_lm_til_lm
+    denumerator <- f.alpha(al) * f.lambda(lm) * tran_al_al_til * tran_lm_lm_til  
+    # print("numerator:")
+    # print(numerator)
+    # print("denumerator:")
+    # print(denumerator)
+    if (is.nan(numerator)){
+      ratio <- 1
+    }
+    else{
+      ratio <- numerator/denumerator  
+    }
+    
+    # print("----ratio---")
+    # print(ratio)
     return (ratio)
   }
   for (i in 2:(N+1)){
